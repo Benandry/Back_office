f74b9fb318812173287a50cd1697cb1599935ea7
Eloi Charly <nandry556@gmail.com>
2024-01-05 08:37:05 +0100

    init: initialisation  back_office

---
+++ src/Repository/StaffRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Staff;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Staff>
+ *
+ * @method Staff|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Staff|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Staff[]    findAll()
+ * @method Staff[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StaffRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Staff::class);
+    }
+
+    //    /**
+    //     * @return Staff[] Returns an array of Staff objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    public function findAllSatff(): array
+    {
+        $columns = [
+            's.id', 's.firstname',
+            's.lastname', 's.birth_',
+            's.number_cin',
+            's.number_matricule',
+            's.adresse_exact',
+            's.work_post',
+        ];
+        return $this->createQueryBuilder('s')
+            ->select($columns)
+            ->getQuery()
+            ->getResult();
+    }
+}
