be7612d50b2e2ce36e1b5b07d2213904f47d602a
Eloi Charly <nandry556@gmail.com>
2025-04-26 10:27:03 +0300

    refactor: remove sensio/framework-extra-bundle and related configuration

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
