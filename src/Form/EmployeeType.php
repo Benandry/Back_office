b8212961dc7f31e431b04d8d84d44ef525f316c4
Eloi Charly <nandry556@gmail.com>
2025-04-27 16:18:45 +0300

    feat: enhance employee form by adding autocomplete for poste and contrat fields, and reorder image upload field

--- src/Form/EmployeeType.php
+++ src/Form/EmployeeType.php
@@ -101,26 +101,27 @@ class EmployeeType extends AbstractType
                 ]
             ])
             ->add('poste', EntityType::class, [
+                'autocomplete' => true,
                 'class' => Poste::class,
                 'choice_label' => 'name',
                 'label' => 'Poste',
                 'label_attr' => [
-                    'class' => 'block text-sm font-medium text-gray-700 mb-4',
+                    'class' => 'block text-sm font-medium text-gray-700 mb-2',
                 ],
                 'attr' => [
-                    'class' => 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 mb-4', // Ajout de mb-4 pour la marge
                     'placeholder' => 'Sélectionnez le poste  ',
+                    'class' => 'mb-8',
                 ]
             ])
             ->add('contrat', EntityType::class, [
                 'class' => Contrat::class,
+                'autocomplete' => true,
                 'choice_label' => 'name',
                 'label' => 'Contrat',
                 'label_attr' => [
-                    'class' => 'block text-sm font-medium text-gray-700 mb-4',
+                    'class' => 'block text-sm font-medium text-gray-700 mb-2',
                 ],
                 'attr' => [
-                    'class' => 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 mb-4', // Ajout de mb-4 pour la marge
                     'placeholder' => 'Sélectionnez le contrat  ',
                 ]
             ])
